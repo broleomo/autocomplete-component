cb50962f41b2218b79460b019edfdca9637bbce5
Brooklynn Moor <brooklynn.moor@mediacurrent.com>
2020-01-29 23:49:11 -0800

    separated out suggestions list from autocomplete, added styling, got data suggestions to filter and update  state with hooks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import Autocomplete from './Autocomplete';
-import logo from './logo.svg';
 import axios from "axios";
 import './App.css';
 
 function App() {
-  const  [data, setData] = useState({ predictions: [] });
-  console.log(data);
-  useEffect(() => {
-  const fetchData = async () => {
-    const result = await axios(
-      'https://coding-challenge.echoandapex.com/locations?q=pdx',
-    );
-    setData(result.data);
-  };
-  fetchData();
-}, []);
-
   return (
-    <div className="container">
-      <h1>Blooming Pixel Creatives Autocomplete</h1>
-      <div className="search-container">
-        <input type="text" className="search-box"/>
-        <button type="submit" className="search-button">Search</button>
+    <>
+      <h1>Blooming Pixels Autocomplete</h1>
+      <div className="container">
+        <Autocomplete />
       </div>
-      <Autocomplete 
-        text="This is the autocomplete component"
-        suggestions={data.predictions}
-      />
-    </div>
+    </>
   );
 }
 
--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,18 +1,65 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, {useState, useEffect} from 'react';
+import SuggestionsList from './SuggestionsList';
+import axios from "axios";
 import './Autocomplete.css';
 
+const Autocomplete = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [selectedSuggestion, setSelectedSuggestion] = useState(0);
+  const [displaySuggestions, setDisplaySuggestions] = useState(false);
+  const  [data, setData] = useState({ predictions: [] });
+  
+  useEffect(() => {
+  const fetchData = async () => {
+    const result = await axios(
+      'https://coding-challenge.echoandapex.com/locations?q=pdx',
+    );
+    setData(result.data);
+  };
+  fetchData();
+}, []);
 
+const formattedPredictions = data.predictions.map(prediction => ({name: prediction.name, description: prediction.description}));
 
-const Autocomplete  = ({suggestions}) => {
-  const suggestionItems = suggestions.map((suggestion) => <li key={suggestion.id}>{suggestion.name}</li>)
+const suggestions = formattedPredictions.map((prediction) => prediction.name);
+
+  const onChange = event => {
+    const value = event.target.value;
+    setInputValue(value);
+
+    const filteredSuggestions = suggestions.filter(suggestion =>
+      suggestion.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setFilteredSuggestions(filteredSuggestions);
+    setDisplaySuggestions(true);
+  };
+
+  const onSelectSuggestion = index => {
+    setSelectedSuggestion(index);
+    setInputValue(filteredSuggestions[index]);
+    setFilteredSuggestions([]);
+    setDisplaySuggestions(false);
+  };
+
   return (
-    <div className="autocomplete-container">
-      <ul>
-        {suggestionItems}
-      </ul>
-    </div>
-    )
-}
+    <>
+      <input
+        className="form-input"
+        type="text"
+        onChange={onChange}
+        value={inputValue}
+      />
+      <SuggestionsList
+        inputValue={inputValue}
+        selectedSuggestion={selectedSuggestion}
+        onSelectSuggestion={onSelectSuggestion}
+        displaySuggestions={displaySuggestions}
+        suggestions={suggestions}
+      />
+    </>
+  );
+};
 
 export default Autocomplete;
